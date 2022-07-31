77822564fdcacfb4109fedc7c670e6caef16f279
lvsoso <wuqize5109@qq.com>
2022-07-31 15:31:45 +0800

    [6] unary op

--- rvcc/main.c
+++ rvcc/main.c
@@ -182,6 +182,7 @@ typedef enum {
     ND_MUL, // *
     ND_DIV, // /
     ND_NUM, // interger
+    ND_NEG, // 负号 -
 } NodeKind;
 
 // AST binary tree's node
@@ -202,6 +203,13 @@ static Node *newNode(NodeKind Kind){
     return Nd;
 }
 
+// new single tree node
+static Node *newUnary(NodeKind Kind, Node *Expr) {
+  Node *Nd = newNode(Kind);
+  Nd->LHS = Expr;
+  return Nd;
+}
+
 // new a binary node
 static Node *newBinary(NodeKind Kind, Node *LHS, Node *RHS) {
   Node *Nd = newNode(Kind);
@@ -218,10 +226,12 @@ static Node *newNum(int Val) {
 }
 
 // expr = mul ("+" mul | "-" mul)*
-// mul = primary ("*" primary | "/" primary)*
+// mul = unary ("*" unary | "/" unary)*
+// unary = ("+" | "-") unary | primary
 // primary = "(" expr ")" | num
 static Node *expr(Token **Rest, Token *Tok);
 static Node *mul(Token **Rest, Token *Tok);
+static Node *unary(Token **Rest, Token *Tok);
 static Node *primary(Token **Rest, Token *Tok);
 
 // expr = mul ("+" mul | "-" mul)*
@@ -249,28 +259,42 @@ static Node *expr(Token **Rest, Token *Tok) {
 }
 
 // 解析乘除
-// mul = primary ("*" primary | "/" primary)*
+// mul = unary ("*" unary | "/" unary)*
 static Node *mul(Token **Rest, Token *Tok) {
-  // primary
-  Node *Nd = primary(&Tok, Tok);
+  // unary
+  Node *Nd = unary(&Tok, Tok);
 
-  // ("*" primary | "/" primary)*
+  // ("*" unary | "/" unary)*
   while (true) {
-    // "*" primary
+    // "*" unary
     if (equal(Tok, "*")) {
-      Nd = newBinary(ND_MUL, Nd, primary(&Tok, Tok->Next));
+      Nd = newBinary(ND_MUL, Nd, unary(&Tok, Tok->Next));
       continue;
     }
 
-    // "/" primary
+    // "/" unary
     if (equal(Tok, "/")) {
-      Nd = newBinary(ND_DIV, Nd, primary(&Tok, Tok->Next));
+      Nd = newBinary(ND_DIV, Nd, unary(&Tok, Tok->Next));
       continue;
     }
 
     *Rest = Tok;
     return Nd;
   }
+}
+
+// unary = ("+" | "-") unary | primary
+static Node *unary(Token **Rest, Token *Tok) {
+  // "+" unary
+  if (equal(Tok, "+"))
+    return unary(Rest, Tok->Next);
+
+  // "-" unary
+  if (equal(Tok, "-"))
+    return newUnary(ND_NEG, unary(Rest, Tok->Next));
+
+  // primary
+  return primary(Rest, Tok);
 }
 
 // 解析括号、数字
@@ -324,12 +348,20 @@ static void pop(char *Reg) {
 
 // 生成表达式
 static void genExpr(Node *Nd) {
-  // 加载数字到a0
-  if (Nd->Kind == ND_NUM) {
-    printf("  li a0, %d\n", Nd->Val);
-    return;
+  switch (Nd->Kind) {
+    // 加载数字到a0
+    case ND_NUM:
+      printf("  li a0, %d\n", Nd->Val);
+      return;
+    // 对寄存器取反
+    case ND_NEG:
+      genExpr(Nd->LHS);
+      // neg a0, a0是 sub a0, x0的别名, a0，即a0=0-a0
+      printf("  neg a0, a0\n");
+      return;
+    default:
+      break;
   }
-
   // 递归到最右节点
   genExpr(Nd->RHS);
   // 将结果压入栈
